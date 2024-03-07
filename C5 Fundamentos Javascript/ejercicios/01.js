b0159cfe9b14882117f5112da6db6dc82f50099c
AlexFargas1 <161654717+AlexFargas1@users.noreply.github.com>
2024-03-07 12:43:19 -0300

    Update 01.js

--- C5 Fundamentos Javascript/ejercicios/01.js
+++ C5 Fundamentos Javascript/ejercicios/01.js
@@ -1,6 +1,6 @@
 // Crea una variable de tipo string.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoString = null;
-
+const aaanuevoString = null;
+ 
 module.exports = nuevoString;
